8eb88116763c053ec108ae9ad43e1d59e303b33c
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-04-25 21:00:52 +0300

    Import User Progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,21 @@
+import { redirect } from "next/navigation";
+
+import { getUserProgress } from "@/db/queries";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
-import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
 
-const LearnPage = () => {
+import { Header } from "./header";
+
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     // flex-row-reverse ??
     <div className='flex flex-row-reverse gap-[48px] px-6'>
