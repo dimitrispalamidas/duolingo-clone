eacc8a6874337a21c14b86a99cfd926e5d69fefb
Dimitris Palamidas <palamidas.dim@gmail.com>
2024-05-29 14:27:19 +0300

    Modified public and scripts folder for production

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -5,11 +5,12 @@ import { neon } from "@neondatabase/serverless";
 import * as schema from "../db/schema";
 
 const sql = neon(process.env.DATABASE_URL!);
+// @ts-ignore
 const db = drizzle(sql, { schema });
 
 const main = async () => {
   try {
-    console.log("Seeding database...");
+    console.log("Seeding database");
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
@@ -18,28 +19,27 @@ const main = async () => {
     await db.delete(schema.challenges);
     await db.delete(schema.challengeOptions);
     await db.delete(schema.challengeProgress);
-    await db.delete(schema.userSubscription);
 
     await db.insert(schema.courses).values([
       {
         id: 1,
         title: "Spanish",
-        imageSrc: "/sp.gif",
+        imageSrc: "/es.svg",
       },
       {
         id: 2,
         title: "Italian",
-        imageSrc: "/it.gif",
+        imageSrc: "/it.svg",
       },
       {
         id: 3,
         title: "French",
-        imageSrc: "/fr.gif",
+        imageSrc: "/fr.svg",
       },
       {
         id: 4,
-        title: "Greek",
-        imageSrc: "/gr.gif",
+        title: "Croatian",
+        imageSrc: "/hr.svg",
       },
     ]);
 
@@ -70,19 +70,19 @@ const main = async () => {
         id: 3,
         unitId: 1, // Unit 1 (Learn the basics...)
         order: 3,
-        title: "Abjectives",
+        title: "Verbs",
       },
       {
         id: 4,
         unitId: 1, // Unit 1 (Learn the basics...)
         order: 4,
-        title: "Adverbs",
+        title: "Verbs",
       },
       {
         id: 5,
         unitId: 1, // Unit 1 (Learn the basics...)
         order: 5,
-        title: "Conjunctions",
+        title: "Verbs",
       },
     ]);
 
@@ -92,7 +92,7 @@ const main = async () => {
         lessonId: 1, // Nouns
         type: "SELECT",
         order: 1,
-        question: 'Which one of these it the "the man"?',
+        question: 'Which one of these is the "the man"?',
       },
       {
         id: 2,
@@ -106,28 +106,28 @@ const main = async () => {
         lessonId: 1, // Nouns
         type: "SELECT",
         order: 3,
-        question: 'Which one of these it the "the robot"?',
+        question: 'Which one of these is the "the robot"?',
       },
     ]);
 
     await db.insert(schema.challengeOptions).values([
       {
         challengeId: 1, // Which one of these is "the man"?
-        imageSrc: "/man.png",
+        imageSrc: "/man.svg",
         correct: true,
         text: "el hombre",
         audioSrc: "/es_man.mp3",
       },
       {
         challengeId: 1,
-        imageSrc: "/woman.png",
+        imageSrc: "/woman.svg",
         correct: false,
         text: "la mujer",
         audioSrc: "/es_woman.mp3",
       },
       {
         challengeId: 1,
-        imageSrc: "/robot.png",
+        imageSrc: "/robot.svg",
         correct: false,
         text: "el robot",
         audioSrc: "/es_robot.mp3",
@@ -157,22 +157,22 @@ const main = async () => {
 
     await db.insert(schema.challengeOptions).values([
       {
-        challengeId: 3, // Which one of these is "the robot"?
-        imageSrc: "/man.png",
+        challengeId: 3, // Which one of these is the "the robot"?
+        imageSrc: "/man.svg",
         correct: false,
         text: "el hombre",
         audioSrc: "/es_man.mp3",
       },
       {
         challengeId: 3,
-        imageSrc: "/woman.png",
+        imageSrc: "/woman.svg",
         correct: false,
         text: "la mujer",
         audioSrc: "/es_woman.mp3",
       },
       {
         challengeId: 3,
-        imageSrc: "/robot.png",
+        imageSrc: "/robot.svg",
         correct: true,
         text: "el robot",
         audioSrc: "/es_robot.mp3",
@@ -182,27 +182,26 @@ const main = async () => {
     await db.insert(schema.challenges).values([
       {
         id: 4,
-        lessonId: 2, // Nouns
+        lessonId: 2, // Verbs
         type: "SELECT",
         order: 1,
-        question: 'Which one of these it the "the man"?',
+        question: 'Which one of these is the "the man"?',
       },
       {
         id: 5,
-        lessonId: 2, // Nouns
+        lessonId: 2, // Verbs
         type: "ASSIST",
         order: 2,
         question: '"the man"',
       },
       {
         id: 6,
-        lessonId: 2, // Nouns
+        lessonId: 2, // Verbs
         type: "SELECT",
         order: 3,
-        question: 'Which one of these it the "the robot"?',
+        question: 'Which one of these is the "the robot"?',
       },
     ]);
-
     console.log("Seeding finished");
   } catch (error) {
     console.error(error);
